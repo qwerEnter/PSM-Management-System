a8266dd7830f4b41709fe221a51922f1c135bda0
nrizzlin <96154798+nrizzlin@users.noreply.github.com>
2023-06-09 14:57:22 +0800

    Delete Manage_Title directory

--- Manage_Title/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "psm_info";
-$bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
-?>
